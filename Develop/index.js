dcaf28d77b414a30d85f60de72893d50f4ad19fc
GgeoffG <121970803+GgeoffG@users.noreply.github.com>
2023-03-22 13:38:25 -0700

    Prototype Questions added

--- Develop/index.js
+++ Develop/index.js
@@ -1,23 +1,89 @@
 // TODO: Include packages needed for this application
 const markdown=require('./utils/generateMarkdown')
+const fs= require('fs')
 const inquirer= require('inquirer')
 // TODO: Create an array of questions for user input
+// const generateMarkdown= ({title,about,builtwith,includeinstall,usage,roadmap,license, username,twitterhandle,github,email}) =>
 inquirer
-.prompt()
-const questions = [
+.prompt(
+[
     {
         type: 'input',
         message: 'What is the Title of your app?',
         name: 'title',
-    }
+    },
     {
-        type: 'input'
-        message: ''
-    }
-];
+        type:'editor',
+        message:'Tell us about your project',
+        name:'about'
+    },
+    {
+        type: 'checkbox',
+        message: 'What frameworks have you used in this project? (check all that apply)',
+        choices: ['Next', 'React','Vue','Angular','Svelte','Laravel','Bootstrap','JQuery'],
+        name: 'builtwith'
+    },
+    {
+        type:'confirm',
+        message:'Would you like to include installationg instructions?',
+        name:'includeinstall',
+        default: false
+    },
+    {
+        type:'editor',
+        message:'Please write your installation instructions',
+        name:'instruct',
+        when:(data) => data.includeinstall===true
+    },
+    {
+        type: 'editor',
+        message: 'Tell us about how to use your project',
+        name:'usage'
+    },
+    {
+        type:"editor",
+        message:"Tell us about future development of this project",
+        name:'roadmap'
+    },
+    {
+        type:"rawlist",
+        message:'Pick a liscence',
+        choices:['MIT','GNU General Public v3','Boost Software License','BSD','Apache','Unlicense'],
+        name: 'license'
+    },
+    {
+        type:'input',
+        message:'Enter your name',
+        name: 'username',
+    },
+    {
+        type:'input',
+        message: 'Enter your twitter handle',
+        name: 'twitterhandle'
+    },
+    {
+        type:'input',
+        message: 'Enter your github accout name',
+        name: 'github'
+    },
+    {
+        type:'input',
+        message: 'Enter your email account',
+        name: 'email'
+    },
+])
+.then( writeJson=(data)=> {
+fs.writeFile('README.json', JSON.stringify(data, null, `\t`), (err) => {
+    err? console.log(err) : console.log("file saved")
+})
+})
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+async function writeToFile(fileName, data) {
+    let info=fs.readFile('README.json');
+    console.log(info)
+}
 
 // TODO: Create a function to initialize app
 function init() {}
---
+++ readmetemplate.md
@@ -0,0 +1,101 @@
+#${data.title}
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#roadmap">Roadmap</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+[![Product Name Screen Shot][product-screenshot]](https://example.com)
+${about}
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+### Built With
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- GETTING STARTED -->
+<!-- ## Getting Started
+
+
+### Prerequisites -->
+
+### Installation
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- ROADMAP -->
+## Roadmap
+
+See the [open issues](https://github.com/othneildrew/Best-README-Template/issues) for a full list of proposed features (and known issues).
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- LICENSE -->
+## License
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Your Name - [@your_twitter](https://twitter.com/your_username) - email@example.com
+
+Project Link: [https://github.com/your_username/repo_name](https://github.com/your_username/repo_name)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>`
